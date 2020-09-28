9dd4c2f56ce6940837dbe984a88f81e0e01427ee
Rogerio Souza <rogerio_dsouza@outlook.com>
2020-09-27 21:43:25 -0300

    Add Welcome1.java
    Implementação do primeiro App Java.

---
+++ src/Capitulo-2-Introducao-app-java/Welcome1.java
@@ -0,0 +1,11 @@
+// Programa de impressão de texto.
+
+public class Welcome1
+{
+    // método main inicia a execução do aplicativo Java
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to Java Programming!");
+    } // fim do método main
+
+} // fim da classe Welcome1
